b2d3e8a69a160dd51699fa10f440856f4ac725fd
Ronan <ronan@getheartbeat.co>
2021-01-25 02:20:50 -0800

    Demo code.

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
+# Simple Hubspot Form Submission
+This is a basic example of how to submit data to a Hubspot form.
+
+##Requirements
+1. A hubspot account.
+2. Create a marketing form in said hubspot account.
+3. Account's Portal ID
+4. The form's GUID
+
+## Hubspot
+For this to work, make sure that a form exists to which the contact data will be submitted. The attributes of that form (meaning things like, contact name, address, email, etc) need to be set up before trying to submit to the form. This means that you'll need to know what attributes of the contact needs to be saved in Hubspot. Also, the name of the values submitted to the Hubspot API will need to match the name of the values when creating the form.
+
+After the form is created, go back to the list of forms and select the new form and click "Actions". Then select "Share". This will open a modal with two options, "Share Link" and "Embed Code". Click on "Embed Code". The code here will include both the Portal ID and the Form ID (Form GUID). Use this information to update the env.js file.
+
+## Install
+Clone this repo to your directory of choice. Then run `npm install` to install the very few packages needed.
+
+Copy the file `env.js.example` and rename it as `env.js`. 
+
+Open the new file `env.js` and update the value for `config.hubspotAccountId` and `config.hubspotFormId` to match the values copied from the appropriate form in Hubspot (see [Hubspot](#Hubspot) section above).
+
+
+## Reference
+https://knowledge.hubspot.com/forms/create-forms
+https://knowledge.hubspot.com/forms/find-your-form-guid
+https://legacydocs.hubspot.com/docs/methods/forms/forms_overview
+https://legacydocs.hubspot.com/docs/methods/forms/submit_form
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
+import React, { Component } from "react";
+
+import config from './env';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props, context) {
+    super(props);
+
+    this.state = {
+      message: '',
+      error: false
+    }
+
+    //this.email = React.createRef();
+    this.name = React.createRef();
+    this.surname = React.createRef();
+    this.email = React.createRef();
+    this.company = React.createRef();
+    this.phone = React.createRef();
+  }
+
+  /**
+   * Submit form data to hubspot.
+   * See https://legacydocs.hubspot.com/docs/methods/forms/submit_form
+   */
+  handleSubmit(){
+    // data object to submit to Hubspot;
+    let data = {};
+
+    // data for data submission
+    data.submittedAt = Date.now();  // unix timestamp
+
+    // user information to be submitted: note that these 'name' values must be created in a form in hubspot for this to work, and the
+    // form should have the GUID set in config object (see env.js)
+    data.fields = [{
+      'name': 'email',
+      'value': this.email.current.value
+     },
+     {
+      'name': 'firstname',
+      'value': this.name.current.value
+     },
+     {
+      'name': 'lastname',
+      'value': this.surname.current.value
+     },
+     {
+      'name': 'company',
+      'value': this.company.current.value
+     },
+     {
+      'name': 'phone',
+      'value': this.phone.current.value
+     }];
+
+     // additional information about where the data is coming from
+     data.context = {
+      'pageUri': 'localhost',
+      'pageName': 'Test Form',
+      //'hutk': ''            // include this parameter and set it to the hubspotutk cookie value to enable cookie tracking on your submission
+     };
+
+
+     // build URL for the api endpoint to post to.
+     let postUrl = config.hubspotEndpoint + config.hubspotAccountId + '/' + config.hubspotFormId;
+
+
+     let req = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+     }
+
+     fetch(postUrl, req).then(async response => {
+        const resp = await response.json();
+
+        if(!response.ok) {
+          const error = (resp && resp.message) || response.status;
+          return Promise.reject(error);
+        } else {
+          this.setState({
+            message: 'Submitted successfully',
+            error: false
+          });
+        }
+
+        console.log(resp);
+     }).catch(error => {
+        console.warn("Error!!!!");
+        console.warn(error.toString());
+
+        this.setState({
+            message: 'Failed to submit',
+            error: true
+        });
+     });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <ul>
+              <li><p style={this.state.error ? {'color':'red'} : {'color':'green'}}>{this.state.message}</p></li>
+              <li>
+                <input type="text" name="first_name" placeholder="Given Name" ref={this.name} />
+              </li>
+              <li>
+                <input type="text" name="last_name" placeholder="Surname" ref={this.surname} />
+              </li>
+              <li>
+                <input type="email" name="email" placeholder="Email" ref={this.email} />
+              </li>
+              <li>
+                <input type="text" name="company_name" placeholder="Company Name" ref={this.company} />
+              </li>
+              <li>
+                <input type="text" name="phone_number" placeholder="Phone Number" ref={this.phone} />
+              </li>
+              <li>
+                <button onClick={this.handleSubmit.bind(this)}>Submit</button>
+              </li>
+            </ul>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
